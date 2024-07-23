cba562fe0e4fa83b545c18845ed1691ba5d72bf2
Amanuel Shetaye <amanuel.shetaye@duke.edu>
2024-07-23 15:48:41 -0500

    finished project

--- main.py
+++ main.py
@@ -4,6 +4,23 @@ import imutils
 import time
 from scipy.signal import find_peaks
 from camera import Camera
+from openai import OpenAI
+import openai   
+import os
+
+def sendMessage(count, bool):
+    if bool:
+        openai.api_key = os.getenv('OPEN_API_KEY)')
+    response = openai.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+    {"role": "system", "content": "Only respond in on senetence. Your job is to tell how good I am at juggling a soccer ball with my feet. I will tell you how many times I just juggled the ball and you will give me feedback. Try and be funny and you can be mean. Include the juggle count early in your response"},
+    {"role": "user", "content": f"I just juggled the ball {count} times."}
+  ],
+    )
+    return (response.choices[0].message.content)
+    
+
 
 # Filter a specified color in an image
 def ball_finder(frame, hsv_lower, hsv_upper):
@@ -25,18 +42,24 @@ def peak_counter(center_):
 blueLower = np.array([100, 50, 50])
 blueUpper = np.array([140, 255, 255])
 
+boo = True
+
 center_ = []
 
 camera = Camera()
 
 # Initialize variables to track the time since the last peak
 last_peak_time = time.time()
-last_detection_time = time.time()
-peak_reset_time = 3  # Reset after 3 seconds without a new peak
+last_detection_time = time.time()  
+peak_reset_time = 3  
+detection_reset_time = 3  
 
 counter = 0
 
-while True:
+chat = ""
+
+tries =10
+while tries != 0:
     time.sleep(0.025)
     
     ret, frame = camera.getframe()
@@ -57,31 +80,50 @@ while True:
         center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
         center_.append(center)
 
-        # Check for peaks
         num_peaks = peak_counter(center_)
         if num_peaks > 0:
             last_peak_time = current_time  # Reset the last peak time
             counter = num_peaks
-
-    else:
-        if current_time - last_detection_time > peak_reset_time:
+        if current_time - last_peak_time > detection_reset_time:
             # If ball hasn't been detected for a while, reset the counter
             counter = 0
             center_ = []  # Clear the center_ list
 
-    # Reset counter if no peaks for a set time
+    else:
+        if current_time - last_detection_time > detection_reset_time and counter != 0:
+            # If ball hasn't been detected for a while, reset the counter
+            chat = sendMessage(counter,boo)
+            boo = False
+            counter = 0
+            center_ = []  # Clear the center_ list
+            tries -= 1
+
     if current_time - last_peak_time > peak_reset_time:
         counter = 0
         center_ = []  # Clear the center_ list
         last_peak_time = current_time  # Reset the timer
 
-    # Display the counter on the frame
     cv2.putText(frame, str(counter), (50, 50), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 2)
+    if chat != "":
+        position = (50, 150)  
+        font = cv2.FONT_HERSHEY_COMPLEX
+        font_scale = 0.5
+        font_thickness = 1
 
-    # Draw the contours if any
+        text_color = (0, 255, 0)  # Green
+        background_color = (0, 0, 0)  # Black for contrast
+
+        (text_width, text_height), baseline = cv2.getTextSize(chat, font, font_scale, font_thickness)
+
+        position = (position[0], position[1] + text_height)
+
+        cv2.rectangle(frame, (position[0], position[1] - text_height), (position[0] + text_width, position[1] + baseline), background_color, -1)
+
+        cv2.putText(frame, chat, position, font, font_scale, text_color, font_thickness)
+
+            # Draw the contours if any
     if cnts:
         cv2.drawContours(frame, [c], -1, (0, 255, 0), 2)
-    
 
     cv2.imshow("Juggler Counter", frame)
     
